0171d3f755e2a1a96d05c5245f87a512301909c1
Asu144 <asupatel2007@gmail.com>
2026-07-10 23:32:59 +0530

    Add files via upload

---
+++ report card.py
@@ -0,0 +1,36 @@
+name = 'Alice'
+print(name)
+print(type(name))
+is_student = True
+print(is_student, type(is_student))
+
+#bit changed..further
+
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, int))
+
+#further changed to
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
+
+
+
